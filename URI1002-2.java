bd0c17c72928d35918a3153ef5b4df1ab5a8e224
Leonardo Dias Carlotto <leo_carlotto@hotmail.com.br>
2018-10-08 13:13:46 -0300

    Create URI1002-2.java

---
+++ URI1002-2.java
@@ -0,0 +1,27 @@
+package myproject;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+
+public class Uri1002 {
+	 private static Scanner scan;
+
+		public static void main(String[] args) throws IOException {
+	 
+	 	scan = new Scanner(System.in);
+	 	
+	 	double R;
+	 	double P = 3.14159;
+	 	double A = 0;
+	 	
+		R = scan.nextDouble();
+	 	
+	 	A = P * (R*R);
+	 	
+	 	System.out.printf("A=" + "%.4f",A);
+	 	System.out.println();
+
+		}
+}
